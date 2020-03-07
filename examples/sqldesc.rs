6ef83bdf13f8e396f5f248435e449d67c8d97b5d
Lauri Paimen <lauri@paimen.info>
2020-03-07 18:41:56 +0200

    Improve command line argument parsing

--- examples/sqldesc.rs
+++ examples/sqldesc.rs
@@ -7,49 +7,75 @@ use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
 use sqlparser::ast::{ Statement, Doc, ObjectName, Ident };
 
-fn doc_table(name: &ObjectName, doc: &Doc) {
-    println!("INSERT INTO sqldesc(type, target, description) VALUES('table', '{}', '{}');", name, doc.doc_string());
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Parses SQL description comments")]
+struct Opt {
+    #[structopt(long, help = "Print debug info while running")]
+    debug: bool,
+
+    #[structopt(parse(from_os_str), help = "SQL file to parse")]
+    input_file: PathBuf,
+
+    #[structopt(long, help = "Disables INSERT commands")]
+    no_insert: bool,
+
+    #[structopt(long, help = "Disables COMMENT commands")]
+    no_comment: bool,
 }
 
-fn doc_column(table: &ObjectName, name: &Ident, doc: &Doc) {
-    println!("INSERT INTO sqldesc(type, target, description) VALUES('column', '{}.{}', '{}');", table, name, doc.doc_string());
+fn doc_table(opt: &Opt, name: &ObjectName, doc: &Doc) {
+    if !opt.no_insert {
+        println!("INSERT INTO sqldesc(type, target, description) VALUES('table', '{}', '{}');", name, doc.doc_string());
+    }
+    if !opt.no_comment {
+        println!("COMMENT ON TABLE {} IS '{}';", name, doc.doc_string());
+    }
 }
 
-
+fn doc_column(opt: &Opt, table: &ObjectName, name: &Ident, doc: &Doc) {
+    if !opt.no_insert {
+        println!("INSERT INTO sqldesc(type, target, description) VALUES('column', '{}.{}', '{}');", table, name, doc.doc_string());
+    }
+    if !opt.no_comment {
+        println!("COMMENT ON COLUMN {}.{} IS '{}';", table, name, doc.doc_string());
+    }
+}
 
 fn main() {
 
-    let filename = std::env::args().nth(1).expect(
-        "No filename provided!\n\n
-        Usage: cargo run --example sqldesc FILENAME.sql"
-    );
-
-    let contents = fs::read_to_string(&filename)
-        .unwrap_or_else(|_| panic!("Unable to read the file {}", &filename));
+    let opt = Opt::from_args();
+    
+    let contents = fs::read_to_string(&opt.input_file)
+        .unwrap_or_else(|_| panic!("Unable to read the file"));
 
     let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
 
     let ast = Parser::parse_sql(&dialect, contents.to_string()).unwrap();
 
-    println!(
-        "Round-trip:\n'{}'",
-        ast
-            .iter()
-            .map(std::string::ToString::to_string)
-            .collect::<Vec<_>>()
-            .join("\n")
-    );
-    println!("Parse results:\n{:#?}", ast);
+    if opt.debug {
+        println!(
+            "Round-trip:\n'{}'",
+            ast
+                .iter()
+                .map(std::string::ToString::to_string)
+                .collect::<Vec<_>>()
+                .join("\n")
+        );
+        println!("Parse results:\n{:#?}", ast);
+    }
 
     for expr in ast.iter() {
         match expr {
             Statement::CreateTable { name, doc, columns, .. } => {
                 if doc.is_useful() {
-                    doc_table(name, doc);
+                    doc_table(&opt, name, doc);
                 }
                 for col in columns.iter() {
                     if col.doc.is_useful() {
-                        doc_column(name, &col.name, &col.doc);
+                        doc_column(&opt, name, &col.name, &col.doc);
                     }
                 }
             }
